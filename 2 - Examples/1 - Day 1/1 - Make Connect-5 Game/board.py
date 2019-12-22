c343492190b4fe64da9b4be570dfe49d361d89cd
Chris Ohk <utilForever@gmail.com>
2019-12-23 02:46:22 +0900

    feat(connect-5): Checking point for placing stone

--- 2 - Examples/1 - Day 1/1 - Make Connect-5 Game/board.py
+++ 2 - Examples/1 - Day 1/1 - Make Connect-5 Game/board.py
@@ -4,6 +4,10 @@ class Board():
         self.num_cols = num_cols
         self._grid = {}
 
+    def place_stone(self, player, point):
+        assert self.is_on_grid(point)
+        assert self._grid.get(point) is None
+
     def is_on_grid(self, point):
         return 1 <= point.row <= self.num_rows and \
             1 <= point.col <= self.num_cols
