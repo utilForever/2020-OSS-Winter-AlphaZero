460a8a2e80c6fda117e254c380c9ace4d2fc63d3
Chris Ohk <utilForever@gmail.com>
2019-12-23 02:17:09 +0900

    feat(connect-5): Utility methods for placing stones

--- 2 - Examples/1 - Day 1/1 - Make Connect-5 Game/board.py
+++ 2 - Examples/1 - Day 1/1 - Make Connect-5 Game/board.py
@@ -3,3 +3,13 @@ class Board():
         self.num_rows = num_rows
         self.num_cols = num_cols
         self._grid = {}
+
+    def is_on_grid(self, point):
+        return 1 <= point.row <= self.num_rows and \
+            1 <= point.col <= self.num_cols
+
+    def get(self, point):
+        string = self._grid.get(point)
+        if string is None:
+            return None
+        return string.color
