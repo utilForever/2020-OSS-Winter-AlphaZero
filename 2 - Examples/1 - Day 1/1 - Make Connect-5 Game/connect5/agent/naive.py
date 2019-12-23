e60650373f930b57a97bdae00ed92870ada8aea4
Chris Ohk <utilForever@gmail.com>
2019-12-24 02:39:32 +0900

    fix: Add missing keyword 'in' and import 'Point'

--- 2 - Examples/1 - Day 1/1 - Make Connect-5 Game/connect5/agent/naive.py
+++ 2 - Examples/1 - Day 1/1 - Make Connect-5 Game/connect5/agent/naive.py
@@ -1,12 +1,13 @@
 import random
-from agent.base import Agent
+from connect5.agent.base import Agent
+from connect5.types import Point
 
 class RandomBot(Agent):
     def select_move(self, game_state):
         """Choose a random valid move that preserves our own eyes."""
         candidates = []
-        for r range(1, game_state.board.num_rows + 1):
-            for c range(1, game_state.board.num_cols + 1):
+        for r in range(1, game_state.board.num_rows + 1):
+            for c in range(1, game_state.board.num_cols + 1):
                 candidate = Point(row=r, col=c)
                 if game_state.is_valid_move(Move.play(candidate)):
                     candidates.append(candidate)
