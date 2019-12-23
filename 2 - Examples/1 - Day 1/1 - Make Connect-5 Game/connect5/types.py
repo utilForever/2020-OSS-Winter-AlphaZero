d03668096122e639abdc0586cb2dbf45268ba666
Chris Ohk <utilForever@gmail.com>
2019-12-24 02:37:45 +0900

    feat(connect-5): Create class 'Point'

--- 2 - Examples/1 - Day 1/1 - Make Connect-5 Game/connect5/types.py
+++ 2 - Examples/1 - Day 1/1 - Make Connect-5 Game/connect5/types.py
@@ -1,4 +1,5 @@
 import enum
+from collections import namedtuple
 
 class Player(enum.Enum):
     black = 1
@@ -7,3 +8,12 @@ class Player(enum.Enum):
     @property
     def other(self):
         return Player.black if self == Player.white else Player.white
+
+class Point(namedtuple('Point', 'row col')):
+    def neighbors(self):
+        return [
+            Point(self.row - 1, self.col),
+            Point(self.row + 1, self.col),
+            Point(self.row, self.col - 1),
+            Point(self.row, self.col + 1),
+        ]
