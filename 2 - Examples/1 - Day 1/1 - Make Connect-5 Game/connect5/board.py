634da445e72f4cbf635f9dc8ba0a4c8790393e4d
Chris Ohk <utilForever@gmail.com>
2019-12-29 02:08:11 +0900

    fix: Correct run-time error (out-of-range, type missing)

--- 2 - Examples/1 - Day 1/1 - Make Connect-5 Game/connect5/board.py
+++ 2 - Examples/1 - Day 1/1 - Make Connect-5 Game/connect5/board.py
@@ -5,21 +5,20 @@ class Board:
     def __init__(self, num_rows, num_cols):
         self.num_rows = num_rows
         self.num_cols = num_cols
-        self._grid = [x[:] for x in [[0] * self.num_cols] * self.num_rows]
+        self._grid = [x[:] for x in [[0] * (self.num_cols + 1)] * (self.num_rows + 1)]
 
     def place_stone(self, player, point):
         assert self.is_on_grid(point)
-        assert self._grid.get(point) is None
+        assert self._grid[point.row][point.col] is None
+        self._grid[point.row][point.col] = player.color
 
     def is_on_grid(self, point):
         return 1 <= point.row <= self.num_rows and \
             1 <= point.col <= self.num_cols
 
     def get(self, point):
-        stone = self._grid[point.row][point.col]
-        if stone is None:
-            return None
-        return stone.color
+        stone_color = self._grid[point.row][point.col]
+        return stone_color
 
 class Move:
     def __init__(self, point=None):
