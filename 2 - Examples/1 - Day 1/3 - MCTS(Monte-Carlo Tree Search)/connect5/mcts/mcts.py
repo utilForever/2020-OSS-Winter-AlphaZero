c42ad0354d90f938ea599eb98ddb52e78cfab649
Chris Ohk <utilForever@gmail.com>
2019-12-30 00:55:08 +0900

    feat(mcts): Create class 'MCTSNode'

---
+++ 2 - Examples/1 - Day 1/3 - MCTS(Monte-Carlo Tree Search)/connect5/mcts/mcts.py
@@ -0,0 +1,38 @@
+import math
+import random
+
+from connect5.types import Player
+
+class MCTSNode(object):
+    def __init__(self, game_state, parent=None, move=None):
+        self.game_state = game_state
+        self.parent = parent
+        self.move = move
+        self.win_counts = {
+            Player.black: 0,
+            Player.white: 0
+        }
+        self.num_rollouts = 0
+        self.children = []
+        self.unvisited_moves = game_state.legal_moves()
+
+    def add_random_child(self):
+        index = random.randint(0, len(self.unvisited_moves) - 1)
+        new_move = self.unvisited_moves.pop(index)
+        new_game_state = self.game_state.apply_move(new_move)
+        new_node = MCTSNode(new_game_state, self, new_move)
+        self.children.append(new_node)
+        return new_node
+
+    def record_win(self, winner):
+        self.win_counts[winner] += 1
+        self.num_rollouts += 1
+
+    def can_add_child(self):
+        return len(self.unvisited_moves) > 0
+        
+    def is_terminal(self):
+        return self.game_state.is_over()
+
+    def winning_frac(self, player):
+        return float(self.win_counts[player]) / float(self.num_rollouts)
