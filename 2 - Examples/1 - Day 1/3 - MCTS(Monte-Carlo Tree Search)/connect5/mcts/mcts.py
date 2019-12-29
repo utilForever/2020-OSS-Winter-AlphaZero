55d857e7900fb79c967ec6fc655b872396365a11
Chris Ohk <utilForever@gmail.com>
2019-12-30 01:08:33 +0900

    feat(mcts): Create class 'MCTSAgent'

--- 2 - Examples/1 - Day 1/3 - MCTS(Monte-Carlo Tree Search)/connect5/mcts/mcts.py
+++ 2 - Examples/1 - Day 1/3 - MCTS(Monte-Carlo Tree Search)/connect5/mcts/mcts.py
@@ -1,6 +1,7 @@
 import math
 import random
 
+from connect5 import agent
 from connect5.types import Player
 
 class MCTSNode(object):
@@ -36,3 +37,77 @@ class MCTSNode(object):
 
     def winning_frac(self, player):
         return float(self.win_counts[player]) / float(self.num_rollouts)
+
+class MCTSAgent(agent.Agent):
+    def __init__(self, num_rounds, temperature):
+        agent.Agent.__init__(self)
+        self.num_rounds = num_rounds
+        self.temperature = temperature
+
+    def select_move(self, game_state):
+        root = MCTSNode(game_state)
+        
+        for i in range(self.num_rounds):
+            node = root
+            while (not node.can_add_child()) and (not node.is_terminal()):
+                node = self.select_child(node)
+
+            if node.can_add_child():
+                node = node.add_random_child()
+
+            winner = self.simulate_random_game(node.game_state)
+
+            while node is not None:
+                node.record_win(winner)
+                node = node.parent
+
+        scored_moves = [
+            (child.winning_frac(game_state.next_player), child.move, child.num_rollouts)
+            for child in root.children
+        ]
+        scored_moves.sort(key=lambda x: x[0], reserve=True)
+        for s, m, n in scored_moves[:10]:
+            print('%s - %.3f (%d)' % (m, s, n))
+
+        best_move = None
+        best_pct = -1.0
+        for child in root.children:
+            child_pct = child.winning_frac(game_state.next_player)
+            if child_pct > best_pct:
+                best_pct = child_pct
+                best_move = child.move
+        print('Select move %s with win pct %.3f' % (best_move, best_pct))
+        return best_move
+
+    def select_child(self, node):
+        total_rollouts = sum(child.num_rollouts for child in node.children)
+        log_rollouts = math.log(total_rollouts)
+
+        best_score = -1
+        best_child = None
+
+        for child in node.children:
+            win_percentage = child.winning_frac(node.game_state.next_player)
+            exploration_factor = math.sqrt(log_rollouts / child.num_rollouts)
+            uct_score = win_percentage + self.temperature * exploration_factor
+
+            if uct_score > best_score:
+                best_score = uct_score
+                best_child = child
+        return best_child
+
+    @staticmethod
+    def simulate_random_game(game):
+        bots = {
+            Player.black: agent.FastRandomBot(),
+            Player.white: agent.FastRandomBot(),            
+        }
+        while not game.is_over():
+            bot_move = bots[game.next_player].select_move(game)
+            game = game.apply_move(bot_move)
+        if game.winner is "Black":
+            return Player.black
+        elif game.winner is "White":
+            return Player.white
+        else:
+            return 0
