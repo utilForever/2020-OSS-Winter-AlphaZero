b26b4a43f043f9b900517552ccbf22a296170b7c
JYPark09 <jyp10987@gmail.com>
2020-01-04 14:16:02 +0900

    feat(alphazero): Create class 'ReplayBuffer'

---
+++ 2 - Examples/2 - Day 2/2 - Alpha Zero/alphazero/replaybuffer.py
@@ -0,0 +1,42 @@
+import torch
+import numpy as np
+
+from connect5.types import Player
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.buffer = []
+
+    def push(self, winner, data):
+        if winner is 'Draw':
+            winner = None
+        else:
+            winner = Player.black if winner == 'Black' else Player.white
+
+        for state, pi, color in data:
+            if winner is None:
+                value = 0
+            else:
+                value = 1 if winner == color else -1
+
+            self.buffer.append((state, pi, value))
+
+        if len(self.buffer) > self.capacity:
+            self.buffer = self.buffer[-self.capacity:]
+
+    def sample(self, size):
+        indicies = np.random.choice(len(self.buffer), size, replace=False)
+
+        states, pis, values = [], [], []
+        for i in indicies:
+            state, pi, value = self.buffer[i]
+
+            states.append(state)
+            pis.append(pi)
+            values.append(value)
+
+        return torch.FloatTensor(np.concatenate(states, axis=0)), torch.FloatTensor(pis).view(size, -1), torch.FloatTensor(values).view(size, 1)                                                                                      
+
+    def __len__(self):
+        return len(self.buffer)
