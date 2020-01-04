a67c62cbc383f7e50168d63c47782bf683f23504
JYPark09 <jyp10987@gmail.com>
2020-01-04 12:42:01 +0900

    feat(alphazero): Create class 'Network'

---
+++ 2 - Examples/2 - Day 2/2 - Alpha Zero/alphazero/network.py
@@ -0,0 +1,52 @@
+import torch
+from torch import nn, optim
+
+from alphazero import preprocess
+
+FILTERS = 128
+
+class Flatten(nn.Module):
+    def __init__(self):
+        nn.Module.__init__(self)
+
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+class Network(nn.Module):
+    def __init__(self, board_size):
+        nn.Module.__init__(self)
+
+        self.feature_extraction = nn.Sequential(
+            nn.Conv2d(preprocess.TENSOR_DIM, FILTERS, 5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(FILTERS, FILTERS, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(FILTERS, FILTERS, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(FILTERS, FILTERS, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(FILTERS, FILTERS, 3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+        self.policy_head = nn.Sequential(
+            nn.Conv2d(FILTERS, 2, 1, padding=0),
+            nn.ReLU(inplace=True),
+            Flatten(),
+            nn.Linear(2 * (board_size ** 2), board_size ** 2)
+        )
+
+        self.value_head = nn.Sequential(
+            nn.Conv2d(FILTERS, 1, 1, padding=0),
+            nn.ReLU(inplace=True),
+            Flatten(),
+            nn.Linear(board_size ** 2, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, 1),
+            nn.Tanh()
+        )
+    
+    def forward(self, x):
+        x = self.feature_extraction(x)
+
+        return self.policy_head(x), self.value_head(x)
